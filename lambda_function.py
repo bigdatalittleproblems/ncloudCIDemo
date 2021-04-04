05edc615a13a79e3637f9284666c16d86ceb041f
Christian <christianramirez.datasci@gmail.com>
2021-04-03 22:19:50 -0700

    update to lambda

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ import json
 def lambda_handler(event, context):
     message = 'Hello World V1'
     print(message)
-    resp={"statusCode": 200,"response":message}
+    resp={"statusCode": 200,"body":message}
     return json.dumps(resp)
