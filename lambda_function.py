cddb13e96af199e773417df2521cc2d9ae6d0e15
Christian <christianramirez.datasci@gmail.com>
2021-04-03 22:44:35 -0700

    update lambda function to include isBase64Encoded

--- lambda_function.py
+++ lambda_function.py
@@ -2,12 +2,13 @@ import json
 # import boto3
 # import os
 def lambda_handler(event, context):
-    message = 'Hello World V1'
+    message = "Hello World V1"
     print(message)
     return json.dumps({
         "statusCode": 200,
         "body": {
             "message": message,
         },
-        "headers":{ 'Access-Control-Allow-Origin' : '*' }
+        "headers":{ 'Access-Control-Allow-Origin' : '*' },  
+        "isBase64Encoded": false
     })
